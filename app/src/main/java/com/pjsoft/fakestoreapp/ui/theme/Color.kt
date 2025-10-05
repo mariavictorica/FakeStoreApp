6307e5ff9bbbd8aa387affc89ef4404e16a54a2e
emvic <emvictoricag@gmail.com>
2025-10-05 16:32:07 -0600

    Modificamos archivos Theme y Color, cambiando la paleta de colores a color verde y crema y personalizando textoss y fondos

---
+++ app/src/main/java/com/pjsoft/fakestoreapp/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.pjsoft.fakestoreapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Color.kt
+val EvergreenShadow = Color(0xFF043222) // verde esmeralda
+val AlmondCream = Color(0xFFF6E9D9)     // crema
+val ForestTint = Color(0xFF0B5135)
+val MistyBeige = Color(0xFFECE5DA)
+val SoftBlack = Color(0xFF1A1A1A)
---
+++ app/src/main/java/com/pjsoft/fakestoreapp/ui/theme/Theme.kt
@@ -0,0 +1,55 @@
+package com.pjsoft.fakestoreapp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = EvergreenShadow,
+    onPrimary = AlmondCream,
+    secondary = ForestTint,
+    background = EvergreenShadow,
+    surface = ForestTint,
+    onBackground = AlmondCream,
+    onSurface = AlmondCream
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = EvergreenShadow,
+    onPrimary = AlmondCream,
+    secondary = ForestTint,
+    background = AlmondCream,
+    surface = MistyBeige,
+    onBackground = SoftBlack,
+    onSurface = SoftBlack
+)
+
+@Composable
+fun FakeStoreAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
